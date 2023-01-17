1db7357859a098ff6a2ba69be99bdb88f197935f
Alexandru <alexdnc22@gmail.com>
2023-01-18 00:54:03 +0200

    work on navigation icon

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -1,77 +1,45 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+import '../styles/Navigation.styles.scss';
+import menuClosedIcon from '../assets/menu-closed.png';
+import menuOpenIcon from '../assets/menu-open.png';
+
+
+
+
 
 const Navigation = () => {
-
-    const [showNav, setShowNav] = useState(false);
-    const [scrollPosition, setScrollPosition] = useState(0);
-
-    const handleScroll = () => {
-        const position = window.pageYOffset;
-        setScrollPosition(position);
-
-        if (position > scrollPosition) {
-            setShowNav(true);
-        } else {
-            setShowNav(false);
-        }
+  const [showNav, setShowNav] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [menuOpen, setMenuOpen] = useState(false);
+ 
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') { 
+      if (window.scrollY > lastScrollY) {
+        setShowNav(false); 
+      } else { 
+        setShowNav(true);  
+      }
+      setLastScrollY(window.scrollY); 
     }
+  };
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll, {passive: true});
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
-        }
-    }, [])
-
-    useEffect(() => {
-        console.log(showNav);
-    }, [showNav])
-
-   
-
-    
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlNavbar);
+      return () => {
+        window.removeEventListener('scroll', controlNavbar);
+      };
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lastScrollY]);
 
     return (
-        <Wrapper>
-           
-            {!showNav && <Icon>
-                <MenuSquare />
-            </Icon>}
-            
-        </Wrapper>
+        <div className='nav-wrapper'>
+           <div className={showNav ? 'visible-nav' : 'not-visible-nav'}>
+                <img src={menuOpen ? menuOpenIcon : menuClosedIcon} id='menu-icon' alt='menu-icon' onClick={() => setMenuOpen(!menuOpen)}  />
+            </div>
+        </div>
     )
 }
-
-const Wrapper = styled.div`
-    background-color: transparent;
-    height: 70px;
-    width: 100%;
-    position: fixed;
-    
-    
-`
-const Icon = styled.div`
-    background-color: white;
-    height: 50px;
-    width: 50px;
-    border-radius: 50%;
-    margin-left: auto;
-    margin-right: 20px;
-    margin-top: 20px;
-    align-items: center;
-    justify-content: center;
-    display: flex;
-    cursor: pointer;
-    
-`
-const MenuSquare = styled.div`
-    background-color: transparent;
-    width: 20px;
-    height: 20px;
-    border: 2px solid black;
-    
-`
 
 export default Navigation;
