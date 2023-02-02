5ff29b0fad901fc4288743e9eea751ad8779116b
Alexandru <alexdnc22@gmail.com>
2023-02-02 22:42:02 +0200

    change the header in the homepage

--- src/screens/Homepage.jsx
+++ src/screens/Homepage.jsx
@@ -1,36 +1,47 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import '../styles/Homepage.styles.scss';
-import headerVideo from '../assets/headerVideo.mp4';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 import { useRef } from 'react';
+import { useEffect } from 'react';
+gsap.registerPlugin(ScrollTrigger);
+
 
 
 
 const Homepage = () => {
-  const videoRef = useRef();
-  const setPlayBack = () => {
-    videoRef.current.playbackRate = 2;
-  }
+  // const headerTitleRef = useRef(null);
+  // const headerRef = useRef(null);
+  // const [tl, setTl] = useState(gsap.timeline({paused: true}))
+
+  // useEffect(() => {
+
+  //   tl.to(headerTitleRef.current, {
+  //     x: '-120%',
+  //     ease: 'ease',
+  //     opacity: 0.01,
+  //     scrollTrigger: {
+  //       trigger: headerRef.current,
+  //       id: 'scroll-in',
+  //       start: "50px 5%",
+  //       end: "1000px 5%",
+  //       scrub: 1.5,
+  //       markers: true,
+  //       toggleActions: 'play none none reverse'
+  //     }
+  //   })
+  // }, [tl])
 
     return (
         <div className='home-wrapper'>
-            
         <div className='home-header'>
-          
-          <h1 className='header-title'>
+          <h1 className='header-title' >
             <span className='header-title-creative'>
               Creative
             </span>
             Front-End Developer
           </h1>
-      <div className='video-wrapper'>
-      <video src={headerVideo} alt='header-video' autoPlay loop controls={false} className='video' ref={videoRef} onCanPlay={() => setPlayBack()} />
-      </div>
-          
-
         </div>
-            
-           
         </div>
     )
 }
