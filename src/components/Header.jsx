a64e7775edea1545aa76e6e9c4e63c1ffe4db18c
Ironessence <alexdnc22@gmail.com>
2023-02-04 17:30:04 +0200

    header animations almost ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,14 @@ import '../styles/Header.styles.scss';
 import bigIDE from '../assets/IDE filled.png';
 import {motion} from 'framer-motion';
 
-const Header = ({ideRef, headerRef}) => {
+const Header = ({ideRef, textRef, specialRef, scrollDownText}) => {
 
     return (
-        <div className='home-header' ref={headerRef} >
+        <div className='home-header'>
            
-            {/* <h1 className="header-title" >Hi there! <br />I'm Alex, <br /> a <span className='header-title-special'>Creative<br/> </span>Front-End Developer</h1> */}
+            <h1 className="header-title" ref={textRef} >Hi there! <br />I'm <span className='header-title-special'>Alex</span>, <br /> a <span className='header-title-special' ref={specialRef}>Creative </span>Front-End Developer</h1>
+
+            
             <img 
             src={bigIDE} 
             alt="IDE" 
@@ -16,6 +18,8 @@ const Header = ({ideRef, headerRef}) => {
             ref={ideRef}
            
             />
+
+        <h1 className='header-title-scroll' ref={scrollDownText}>Scroll down to enter my small world</h1>
             
             
         </div>
--- src/screens/Homepage.jsx
+++ src/screens/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import '../styles/Homepage.styles.scss';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
@@ -7,7 +7,9 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Homepage = () => {
  const ideRef = useRef(null);
- const headerRef = useRef(null);
+ const textRef = useRef(null);
+ const specialRef = useRef(null);
+ const scrollDownText = useRef(null);
 
 //PIN IDE TO THE TOP WHEN SCROLLING
  useEffect(() => {
@@ -15,33 +17,20 @@ const Homepage = () => {
 //INSIDE CONTEXT >
     gsap.timeline()
     .to(ideRef.current, {
+      x: -200,
+      scale: 3,
+      duration: 10,
       scrollTrigger: {
         trigger: ideRef.current,
         start: "top 190",
-        end: '+=5000',
+        end: '+=2000',
         scrub: true,
         pin:true,
         anticipatePin: 1,
         markers: true,
       }
     })
-    .to(ideRef.current, {
-      x: -300,
-      scale: 0.5,
-      skewX: -3,
-      rotateZ: -3,
-      rotateX: -3,
-      duration: 0.5,
-      scrollTrigger: {
-        trigger: '.home-wrapper',
-        start: "700 300",
-        end: '+=500',
-        scrub: true,
-        markers: true,
-      }
-    })
-    
-  }, headerRef)
+  }, '.home-wrapper')
 //Clean-Up
   return () => ctx.revert(); 
  }, [])
@@ -49,21 +38,63 @@ const Homepage = () => {
 
  //INTRO ANIMATION FOR IDE
  useEffect(() => {
-  let ctx2 = gsap.context(() => {
+  let ctx = gsap.context(() => {
     //INSIDE CONTEXT >
-       gsap.fromTo(ideRef.current, {y: 200}, {y: 0, duration: 1})
-      }, headerRef)
+       gsap.fromTo(ideRef.current, {y: 200, opacity: 0}, {y: 0, opacity: 1, duration: 1})
+      }, '.home-wrapper')
 
-      return () => ctx2.revert();
+      return () => ctx.revert();
+ }, [])
+
+ //Text animation for appearing
+ useEffect(() => {
+  let ctx = gsap.context(() => {
+    //INSIDE CONTEXT >
+       gsap.fromTo(textRef.current, {height: '0%',  opacity: 0}, {height: '100%', opacity: 1,  duration: 0.5, delay: 1})
+      }, '.home-wrapper')
+
+      return () => ctx.revert();
+ }, [])
+
+// Text controll on scroll
+ useLayoutEffect(() => {
+  let ctx = gsap.context(() => {
+//INSIDE CONTEXT >
+    gsap.timeline()
+    .to(textRef.current, {
+      scale: 2,
+      y: 300,
+      duration: 2,
+      scrollTrigger: {
+        trigger: textRef.current,
+        start: "-190 190",
+        end: '+=400',
+        scrub: true,
+        markers: true,
+      }
+    })
+    
+  }, '.home-wrapper')
+//Clean-Up
+  return () => ctx.revert(); 
  }, [])
 
 
+ //Scroll Down Text animation for appearing
+ useEffect(() => {
+  let ctx = gsap.context(() => {
+    //INSIDE CONTEXT >
+       gsap.fromTo(scrollDownText.current, {y: -200, opacity: 0}, {y: 0, opacity: 1, duration: 1, delay: 1.25})
+      }, '.home-wrapper')
+
+      return () => ctx.revert();
+ }, [])
 
 
  
     return (
         <div className='home-wrapper'>
-          <Header ideRef={ideRef} headerRef={headerRef}  />
+          <Header ideRef={ideRef} textRef={textRef} specialRef={specialRef} scrollDownText={scrollDownText}  />
         </div>
     )
 }
