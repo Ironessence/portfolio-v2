eb05d6bda7f8a4475bcd8742e6411504d465d3a1
Ironessence <alexdnc22@gmail.com>
2023-02-04 15:21:03 +0200

    making gsap wwork

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,14 +8,13 @@ const Header = ({ideRef, headerRef}) => {
     return (
         <div className='home-header' ref={headerRef} >
            
-            <h1 className="header-title" >Hi there! <br />I'm Alex, <br /> a <span className='header-title-special'>Creative<br/> </span>Front-End Developer</h1>
+            {/* <h1 className="header-title" >Hi there! <br />I'm Alex, <br /> a <span className='header-title-special'>Creative<br/> </span>Front-End Developer</h1> */}
             <img 
             src={bigIDE} 
             alt="IDE" 
             className='IDE' 
             ref={ideRef}
-            // initial={{scale: 0.1, rotateZ: 20, opacity: 0, y: 200, transition: {duration: 1, type: 'just'}}}
-            // animate={{scale: 1, rotateZ: 2, opacity: 1, y: 0, skewX: 3, rotateX: -5,  transition: {duration: 1, delay: 1, type: 'spring'}}}
+           
             />
             
             
--- src/screens/Homepage.jsx
+++ src/screens/Homepage.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from 'react';
 import '../styles/Homepage.styles.scss';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
-
 import Header from '../components/Header';
 gsap.registerPlugin(ScrollTrigger);
 
@@ -10,9 +9,56 @@ const Homepage = () => {
  const ideRef = useRef(null);
  const headerRef = useRef(null);
 
-//  gsap.defaults({overwrite: 'auto'});
+//PIN IDE TO THE TOP WHEN SCROLLING
+ useEffect(() => {
+  let ctx = gsap.context(() => {
+//INSIDE CONTEXT >
+    gsap.timeline()
+    .to(ideRef.current, {
+      scrollTrigger: {
+        trigger: ideRef.current,
+        start: "top 190",
+        end: '+=5000',
+        scrub: true,
+        pin:true,
+        anticipatePin: 1,
+        markers: true,
+      }
+    })
+    .to(ideRef.current, {
+      x: -300,
+      scale: 0.5,
+      skewX: -3,
+      rotateZ: -3,
+      rotateX: -3,
+      duration: 0.5,
+      scrollTrigger: {
+        trigger: '.home-wrapper',
+        start: "700 300",
+        end: '+=500',
+        scrub: true,
+        markers: true,
+      }
+    })
+    
+  }, headerRef)
+//Clean-Up
+  return () => ctx.revert(); 
+ }, [])
 
-//  gsap.set(ideRef.current, {xPercent: -50, yPercent: -50});
+
+ //INTRO ANIMATION FOR IDE
+ useEffect(() => {
+  let ctx2 = gsap.context(() => {
+    //INSIDE CONTEXT >
+       gsap.fromTo(ideRef.current, {y: 200}, {y: 0, duration: 1})
+      }, headerRef)
+
+      return () => ctx2.revert();
+ }, [])
+
+
+
 
  
     return (
