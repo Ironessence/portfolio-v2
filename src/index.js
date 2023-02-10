99037b6ce0619c677334625bb3b926fa5292a793
Alexandru <alexdnc22@gmail.com>
2023-02-10 22:17:23 +0200

    threejs added

--- src/index.js
+++ src/index.js
@@ -4,22 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import ErrorPage from './screens/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <ErrorPage />,
   },
-  
-])
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/screens/Homepage.jsx
+++ src/screens/Homepage.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
-
 import styled from 'styled-components';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Model } from '../components/LetterA';
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Homepage = () => {
-  return <Main></Main>;
+  return (
+    <Main>
+      <Canvas>
+        <OrbitControls enableZoom={true} />
+        <ambientLight intensity={0.5} />
+        <directionalLight
+          position={[-2, 5, 2]}
+          intensity={1}
+        />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+      </Canvas>
+    </Main>
+  );
 };
 
 const Main = styled.div`
