e0f9f4d9265e2ba09de19e45e9b38f278276790f
Alexandru <alexdnc22@gmail.com>
2023-02-12 00:42:23 +0200

    work on letter entering

--- src/screens/Hero.jsx
+++ src/screens/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
@@ -6,31 +6,53 @@ import styled from 'styled-components';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { Model } from '../components/LetterA';
-
 gsap.registerPlugin(ScrollTrigger);
 
-function Rig({ scrollPosition }) {
+const Rig = () => {
   const { camera, mouse } = useThree();
   const vec = new THREE.Vector3();
-  return useFrame(() =>
-    camera.position.lerp(vec.set(mouse.x * 1, mouse.y * 1, camera.position.z), 0.01),
-  );
-}
+
+  //Animation for camera to enter through the letter
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      //INSIDE CONTEXT >
+      gsap.timeline().to(camera.position, {
+        z: 4.2,
+        y: 0,
+        x: 0,
+        duration: 1.5,
+        scrollTrigger: {
+          trigger: '.hero',
+          start: 'top top',
+          end: '+=2000',
+          scrub: true,
+          markers: true,
+        },
+      });
+    }, '.hero');
+    //Clean-Up
+    return () => ctx.revert();
+  }, []);
+
+  return useFrame(() => {
+    camera.position.lerp(vec.set(mouse.x * 1, mouse.y * 1, camera.position.z), 0.005);
+  });
+};
 
 const Hero = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const handleScroll = () => {
-    const position = window.pageYOffset;
-    setScrollPosition(position);
-  };
+  // const [scrollPosition, setScrollPosition] = useState(0);
+  // const handleScroll = () => {
+  //   const position = window.pageYOffset;
+  //   setScrollPosition(position);
+  // };
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true });
+  // useEffect(() => {
+  //   window.addEventListener('scroll', handleScroll, { passive: true });
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
   return (
     <Main>
       <Canvas camera={{ position: [0, 0, 5] }}>
@@ -50,7 +72,7 @@ const Hero = () => {
         <Suspense fallback={null}>
           <Model />
         </Suspense>
-        {scrollPosition < 900 && <Rig scrollPosition={scrollPosition} />}
+        <Rig />
       </Canvas>
     </Main>
   );
